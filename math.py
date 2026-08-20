f6ebaf20e5168b6187b06cc886d155d9c8e5f746
khanalsushil226-del <khanalsushil226@gmail.com>
2026-08-20 22:44:38 +0545

    relational operators in python

--- math.py
+++ math.py
@@ -1,8 +1,13 @@
 a = 5
-b = 9 
-sum = a+b
-difference = a-b
-product = a*b
-division = a/b
-all = ((a+b)*b)+a
-print("everything is", all)
+b = 2
+# sum = a+b
+# difference = a-b
+# product = a*b
+# division = a/b
+# all = ((a+b)*b)+a
+# print("everything is", all)
+print (a != b)
+print (a > b)
+print (a >= b) 
+print(a < b)
+print( a <= b)
