d887bfb6cbb8a4dec6a6afe8b2899d5e9504d7b7
khanalsushil226-del <khanalsushil226@gmail.com>
2026-08-21 10:13:31 +0545

    simple interest with python

---
+++ simple.py
@@ -0,0 +1,5 @@
+P = float(input("enter the principal amount: "))
+T = float(input("enter the time: "))
+R = float(input("enter the interest rate: "))
+SI= (P*T*R)/100
+print("simple interest is: Rs.", SI)
