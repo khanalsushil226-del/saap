42882436af905cce1a2caa0d1f829cb4da951b26
khanalsushil226-del <khanalsushil226@gmail.com>
2026-08-21 20:03:19 +0545

    if else condition

---
+++ grade.py
@@ -0,0 +1,10 @@
+marks = int(input("enter marks you scored:"))
+if(marks >= 90):
+    print("grade = A+")
+elif( marks >=80):
+    print("grade = A")
+elif( marks >=70):
+    print("grade = B")
+else:
+    print("work hard next time")
+
