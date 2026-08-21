21977378b5c88d6a4af29e7acfe8460245bfcf9d
khanalsushil226-del <khanalsushil226@gmail.com>
2026-08-21 17:37:59 +0545

    traffic light with conditional statement

---
+++ if.py
@@ -0,0 +1,11 @@
+light = "yellow"
+if(light == "green"):
+    print("you can go")
+elif(light == "red"):
+    print("stop until green")
+elif(light == "yellow"):
+    print("get ready to move")
+else:
+    print("light is broken wait for traffic police to symbol")
+    
+
