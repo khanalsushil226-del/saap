f2985a07668115d9c94afa155fa4a8ece0715a78
khanalsushil226-del <khanalsushil226@gmail.com>
2026-08-21 07:54:52 +0545

    second question

--- practice1.py
+++
@@ -1,4 +0,0 @@
-num1 = int(input("enter the first number: ")
-           num2 = int(input("enter second number:")))
-sum  = num1 + num2
-print("sum of two numbers is: ", sum)
---
+++ practice2.py
@@ -0,0 +1,3 @@
+a = int(input("enter the side of a square: "))
+square = int(a) * int(a)
+print("area of square is: ", square)
