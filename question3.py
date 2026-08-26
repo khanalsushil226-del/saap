b0f45a64c5a4e6634fce3153798b8d30184a75a5
khanalsushil226-del <khanalsushil226@gmail.com>
2026-08-26 07:24:16 +0545

    classworks

--- question3.py
+++ question3.py
@@ -1,3 +1,16 @@
-for i in range(1,6):
-    print(i, end=" ")
-print("hello  world")
+a =  int(input("enter the first value: "))
+b = int(input("enter the second value: "))
+operation = input("enter operation (+,-,*,/)")
+# print(operation)
+if operation == "+" :
+    print(a+b)
+elif operation == "-":
+    print(a-b)
+elif operation == "*" :
+    print(a*b)
+elif operation == "/" :
+    print(a/b)
+else: 
+    print("enter valid operator")
+    
+ 
